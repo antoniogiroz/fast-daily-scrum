cddf581f4eda3d207f0cfae760947c337153a08a
Antonio Gil <antonioluisgil@gmail.com>
2021-02-13 19:42:38 +0100

    Remove warning messages when build

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,18 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   theme: {
     extend: {
       fontSize: {
         '8xl': '6rem',
-        '10xl': '8rem'
-      }
-    }
+        '10xl': '8rem',
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
