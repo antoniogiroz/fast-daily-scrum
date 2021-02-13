fac9576e7675510df011894624dfeb6cf05636e4
Antonio Gil <antonioluisgil@gmail.com>
2021-02-13 09:56:21 +0100

    Extract firebase connection settings to env file

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,9 +2,17 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 if (!firebase.apps.length) {
-  firebase
-    .initializeApp({ projectId: 'fast-daily' })
-    .firestore({ timestampsInSnapshots: true })
+  const config = {
+    apiKey: process.env.DB_API_KEY,
+    authDomain: process.env.DB_AUTH_DOMAIN,
+    databaseURL: process.env.DB_DATABASE_URL,
+    projectId: process.env.DB_PROJECT_ID,
+    storageBucket: process.env.DB_STORAGE_BUCKET,
+    messagingSenderId: process.env.DB_MESSAGING_SENDER_ID,
+    appId: process.env.DB_APP_ID,
+  }
+
+  firebase.initializeApp(config).firestore({ timestampsInSnapshots: true })
 }
 
 export const db = firebase.firestore()
